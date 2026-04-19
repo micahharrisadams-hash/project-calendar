d97561e6c2139f901bcbab310fb71a752d7a5261
micahharrisadams-hash <micahharrisadams@gmail.com>
2026-04-19 01:21:35 -0500

    Update page.tsx

--- project calendar/app/page.tsx
+++ project calendar/app/page.tsx
@@ -1,8 +1,9 @@
+import { StudyPlanner } from "@/components/study-planner"
+
 export default function Home() {
   return (
-    <main style={{padding:20}}>
-      <h1>Study Planner</h1>
-      <p>Your deployment works </p>
+    <main>
+      <StudyPlanner />
     </main>
   )
 }
